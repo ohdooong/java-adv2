8ab5e7d1fdd2204b689ac61234dd1c2487e926c6
ohseunghwan <seunghwan.oh13@hansol.com>
2025-07-22 13:02:34 +0900

    initial commit 3

--- src/charset/EncodingMain1.java
+++ src/charset/EncodingMain1.java
@@ -1,6 +1,10 @@
 package charset;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static java.nio.charset.StandardCharsets.*;
 
 public class EncodingMain1 {
 	
@@ -9,12 +13,20 @@ public class EncodingMain1 {
 	
 	
 	public static void main(String[] args) {
-		
+		System.out.println("== ASCII 영문 처리");
+		encoding("A", US_ASCII);
+		encoding("A", ISO_8859_1);
+		encoding("A", EUC_KR);
+		encoding("A", UTF_8);
+		encoding("A", UTF_16BE);
+
+		System.out.println("== 한글 지원 ==");
+		encoding("가", EUC_KR);
 	}
-	
+
 	
 	private static void encoding(String text, Charset charset) {
 		byte[] bytes = text.getBytes(charset);
-		System.out.printf();
+		System.out.printf("%s -> [%s] 인코딩 -> %s %sbyte\n", text, charset.name(), Arrays.toString(bytes), bytes.length);
 	}
 }
