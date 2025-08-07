2ef231276eac91fd7ec1a0cb365e205c5c8029c8
unknown <seunghwan.oh13@hansol.com>
2025-08-07 12:43:06 +0900

    네트워크 프로그램

--- src/network/tcp/v2/ClientV2.java
+++ src/network/tcp/v2/ClientV2.java
@@ -1,14 +1,15 @@
-package network.tcp.v1;
+package network.tcp.v2;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 import static util.MyLogger.log;
 
-public class ClientV1 {
-    private static final int PORT = 70000;
+public class ClientV2 {
+    private static final int PORT = 12345;
 
     public static void main(String[] args) throws IOException {
         log("클라이언트 시작");
@@ -19,14 +20,26 @@ public class ClientV1 {
 
         log("소켓 연결: " + socket);
 
-        // 서버에게 문자 보내기
-        String toSend = "Hello";
-        output.writeUTF(toSend);
-        log("client -> server: " + toSend);
+        Scanner scanner = new Scanner(System.in);
 
-        // 서버로부터 문자 받기
-        String received = input.readUTF();
-        log("server -> client: " + received);
+        while (true) {
+            System.out.print("전송 문자: ");
+            String toSend = scanner.nextLine();
+
+
+            // 서버에게 문자 보내기
+            output.writeUTF(toSend);
+            log("client -> server: " + toSend);
+
+            if (toSend.equals("exit")) {
+                break;
+            }
+
+            // 서버로부터 문자 받기
+            String received = input.readUTF();
+            log("server -> client: " + received);
+        }
+
 
         // 자원정리
         log("연결 종료: " + socket);
